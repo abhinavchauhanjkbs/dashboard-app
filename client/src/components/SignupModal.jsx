dc5f51b2b016c3d3ab9abed76d13505f83408b9f
Abhinav <chauhanabhiji@gmail.com>
2025-07-16 04:49:57 +0530

    Ready to deploy frontend

--- client/src/components/SignupModal.jsx
+++ client/src/components/SignupModal.jsx
@@ -24,7 +24,7 @@ function SignupModal() {
     }
 
     try {
-      const res = await axios.post('http://localhost:5000/auth/signup', {
+        const res = await axios.post(`${import.meta.env.VITE_API_URL}/auth/signup`, {
         email,
         password,
       });
